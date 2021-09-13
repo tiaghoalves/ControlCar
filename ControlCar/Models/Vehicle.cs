fbc6c4780cdf131c9b3a7019b97f351ed1a0ddf3
marianasm.26@hotmail.com <marianasm.26@hotmail.com>
2021-09-13 20:50:53 -0300

    ok

--- ControlCar/Models/Vehicle.cs
+++ ControlCar/Models/Vehicle.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -16,18 +18,46 @@ namespace ControlCar.Models
         }
 
         public int IdVehicle { get; set; }
+
+        [Display(Name = "Modelo")]
+        [Required(ErrorMessage = "Modelo é obrigatório")]
         public string Model { get; set; }
+
+        [Display(Name = "KM")]
+        [Required(ErrorMessage = "KM é obrigatório")]
         public double Km { get; set; }
+
+        [Display(Name = "Placa")]
         public string Board { get; set; }
+
+        [Display(Name = "Tipo")]
         public string Type { get; set; }
+
+        [Display(Name = "Marca")]
         public string Brand { get; set; }
+
+        [Display(Name = "Cor")]
         public string Color { get; set; }
+
+        [Display(Name = "Ano")]
+        [Required(ErrorMessage = "Ano é obrigatório")]
         public DateTime Year { get; set; }
+
+        [Display(Name = "Renavam")]
+        [Required(ErrorMessage = "Número do Renavam é obrigatório")]
         public decimal Renavam { get; set; }
+
+        [Display(Name = "Chassi")]
+        [Required(ErrorMessage = "Número do chassi é obrigatório")]
         public decimal Chassi { get; set; }
+
+        [Display(Name = "Status Veículo")]
         public int? IdStatusVehicle { get; set; }
+
+        [Display(Name = "Observação")]
         public string Observation { get; set; }
 
+        [Display(Name = "Status")]
         public virtual StatusVehicle IdStatusVehicleNavigation { get; set; }
         public virtual ICollection<Scheduling> Scheduling { get; set; }
         public virtual ICollection<VehicleMaintenance> VehicleMaintenance { get; set; }
