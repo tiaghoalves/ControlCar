2cc194822942ac4943365e17bab916d8228267a7
Tiago Alves <tiago.alves718@gmail.com>
2021-09-23 21:09:02 -0300

    Add routes

---
+++ ControlCar/Controllers/RoutesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ControlCar.Models;
+
+namespace ControlCar.Controllers
+{
+    public class RoutesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public RoutesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Routes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Route.ToListAsync());
+        }
+
+        // GET: Routes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var route = await _context.Route
+                .FirstOrDefaultAsync(m => m.IdRoute == id);
+            if (route == null)
+            {
+                return NotFound();
+            }
+
+            return View(route);
+        }
+
+        // GET: Routes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Routes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdRoute,Source,Destiny,KmPattern")] Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(route);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(route);
+        }
+
+        // GET: Routes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var route = await _context.Route.FindAsync(id);
+            if (route == null)
+            {
+                return NotFound();
+            }
+            return View(route);
+        }
+
+        // POST: Routes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdRoute,Source,Destiny,KmPattern")] Route route)
+        {
+            if (id != route.IdRoute)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(route);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RouteExists(route.IdRoute))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(route);
+        }
+
+        // GET: Routes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var route = await _context.Route
+                .FirstOrDefaultAsync(m => m.IdRoute == id);
+            if (route == null)
+            {
+                return NotFound();
+            }
+
+            return View(route);
+        }
+
+        // POST: Routes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var route = await _context.Route.FindAsync(id);
+            _context.Route.Remove(route);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RouteExists(int id)
+        {
+            return _context.Route.Any(e => e.IdRoute == id);
+        }
+    }
+}
